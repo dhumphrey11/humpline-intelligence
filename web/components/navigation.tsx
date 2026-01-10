5e654c1a3ae5097dac200e32f14441b7e07e0371
davy <dhumphrey11@gmail.com>
2026-01-09 17:25:57 -0700

    Add compact admin nav cluster

--- web/components/navigation.tsx
+++ web/components/navigation.tsx
@@ -3,12 +3,16 @@ import Link from 'next/link';
 export function Navigation() {
   return (
     <nav className="nav-sub">
-      <Link className="nav-link" href="/">Current Portfolio</Link>
-      <Link className="nav-link" href="/models">Historical Models</Link>
-      <Link className="nav-link" href="/transactions">Historical Transactions</Link>
-      <Link className="nav-link" href="/monitoring">Performance Monitoring</Link>
-      <Link className="nav-link" href="/admin">Admin</Link>
-      <Link className="nav-link" href="/admin/data">Data Explorer</Link>
+      <div className="nav-main">
+        <Link className="nav-link" href="/">Current Portfolio</Link>
+        <Link className="nav-link" href="/models">Historical Models</Link>
+        <Link className="nav-link" href="/transactions">Historical Transactions</Link>
+        <Link className="nav-link" href="/monitoring">Performance Monitoring</Link>
+      </div>
+      <div className="nav-admin">
+        <Link className="nav-link" href="/admin">Admin</Link>
+        <Link className="nav-link" href="/admin/data">Data</Link>
+      </div>
     </nav>
   );
 }
